80507834adba67b2018d254b2c455af0eba52416
Spurthi-Ravula <spurthiravula26@gmail.com>
2023-04-23 22:15:08 -0500

    secured bonus

--- routes/vechicle.js
+++ routes/vechicle.js
@@ -17,16 +17,16 @@ const secured = (req, res, next) => {
 router.get('/', vechicle_controlers.vechicle_view_all_Page);
 
 /* GET detail vechicle page */
-router.get('/detail', vechicle_controlers.vechicle_view_one_Page);
+router.get('/detail', secured, vechicle_controlers.vechicle_view_one_Page);
 
 /* GET create vechicle page */
-router.get('/create', vechicle_controlers.vechicle_create_Page);
+router.get('/create', secured, vechicle_controlers.vechicle_create_Page);
 
 /* GET create update page */
 router.get('/update', secured, vechicle_controlers.vechicle_update_Page);
 
 /* GET delete vechicle page */
-router.get('/delete', vechicle_controlers.vechicle_delete_Page);
+router.get('/delete', secured, vechicle_controlers.vechicle_delete_Page);
 
 
 module.exports = router;
